91d9754c84fe13d04228ad1c0ec51c26d4f8c479
Yash Mudgal <96115337+Mudgal2001@users.noreply.github.com>
2023-06-29 10:04:41 +0530

    jwt authentication done

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 router.post("/register", async (req,res)=>{
    try{
+      const saltRounds = 10;
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashPass = bcrypt.hashSync(req.body.password,salt);
         const newUser = new User({
             name:req.body.name,
             email:req.body.email,
-            password:req.body.password,
+            password:hashPass,
         });
         const user1 = await newUser.save();
         res.status(200).json(user1);
@@ -16,5 +20,35 @@ router.post("/register", async (req,res)=>{
     } 
     //console.log(req.body);
 });
-
+ router.post("/login",async(req,res)=>{
+    try{
+     const user = await User.findOne({name:req.body.name});
+     !user && res.status(404).json("worng credentials");
+     const validated = await bcrypt.compareSync(req.body.password, user.password);
+     !validated && res.status(404).json("wrong password");
+     
+     
+        //Creating jwt token
+     const  token = jwt.sign(
+          { userId:user.id,email: user.email },
+          process.env.SECRET_KEY,
+          { expiresIn: "1h" }
+        );
+     const { password,...others } = user._doc;
+     res.status(200).json({user,token});
+    }catch(err){
+       res.status(500).json(err);
+    }
+ });
+ router.get('/accessResource', (req, res)=>{  
+    const token = req.headers.authorization.split(' ')[1]; 
+    //Authorization: 'Bearer TOKEN'
+    if(!token)
+    {
+        res.status(200).json({success:false, message: "Error! Token was not provided."});
+    }
+    //Decoding the token
+    const decodedToken = jwt.verify(token,process.env.SECRET_KEY );
+    res.status(200).json({success:true});   
+})
 module.exports  = router;
